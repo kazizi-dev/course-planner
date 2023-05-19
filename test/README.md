274b4f8a3baf01a65af301721d89565e442e88d9
kazizi-dev <kouroshazizi2012@gmail.com>
2023-05-19 10:58:33 -0700

    update files

--- test/README.md
+++
@@ -1,5 +0,0 @@
-# course-planner
-A course planner web application to process university enrollment data.
-
-## Technologies:
-- Java and Spring Framework
--- test/src/main/java/ca/cmpt213/as5/exception/DepartmentNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package ca.cmpt213.as5.exception;
-
-public class DepartmentNotFoundException extends RuntimeException {
-    public DepartmentNotFoundException() {
-        super("Department not found");
-    }
-}
